c374a08f383237056ced16ae5a656a25afbff713
Nemanja Milanović <neca1mesto@gmail.com>
2023-06-04 12:06:45 +0200

    Clean up design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule,
     FontAwesomeModule
   ],
---
+++ src/app/login-signup.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginSignupService {
+
+  constructor(private http: HttpClient) { }
+
+  uri = 'http://localhost:4000';
+
+  login(formUsername, formPassword){
+    const data = {
+        username: formUsername,
+        password: formPassword
+    }
+
+    return this.http.post(`${this.uri}/users/login`, data);
+  }
+
+  register(formData){
+    return this.http.post(`${this.uri}/users/register`, {...formData});
+  }
+}
